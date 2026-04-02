77bc6b81c3357aa68b6a1171a00b45a8e5dc7642
Thanh Lam <thanhlam0290@gmail.com>
2026-04-02 17:38:22 +0700

    Update

--- hubfacedownload.sh
+++ hubfacedownload.sh
@@ -86,6 +86,11 @@ hf download black-forest-labs/FLUX.2-klein-9b-fp8 \
   flux-2-klein-9b-fp8.safetensors \
   --local-dir $BASE/diffusion_models
 
+#https://huggingface.co/black-forest-labs/FLUX.2-klein-9b-fp8/resolve/main/flux-2-klein-9b-fp8.safetensors?download=true
+hf download black-forest-labs/FLUX.2-klein-9b-fp8 \
+  flux-2-klein-9b-fp8.safetensors \
+  --local-dir $BASE/diffusion_models
+
 #https://huggingface.co/jackzheng/flux-fill-FP8/blob/main/fluxFillFP8_v10.safetensors
 # hf download jackzheng/flux-fill-FP8 \
 #   fluxFillFP8_v10.safetensors \
