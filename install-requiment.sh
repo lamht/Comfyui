056479d48d5ad3bf35e439ffbea1f242d4384eb9
Thanh Lam <thanhlam0290@gmail.com>
2026-04-06 08:51:43 +0700

    Update

--- install-requiment.sh
+++ install-requiment.sh
@@ -12,8 +12,8 @@ echo "Using ComfyUI at: $COMFY_PATH"
 # ==============================
 # DOWNLOAD NODES
 # ==============================
-wget -O custom_nodes.zip "https://www.dropbox.com/scl/fi/molqh8osl8u1i9jyc3rv9/custom_nodes.zip?dl=1"
-unzip -o custom_nodes.zip -d "$COMFY_PATH"
+# wget -O custom_nodes.zip "https://www.dropbox.com/scl/fi/molqh8osl8u1i9jyc3rv9/custom_nodes.zip?dl=1"
+# unzip -o custom_nodes.zip -d "$COMFY_PATH"
 
 # ==============================
 # ACTIVATE VENV
