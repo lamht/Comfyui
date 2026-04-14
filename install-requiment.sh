6541823b6907fe5300ab0529861c9f9fcaab6d0e
Thanh Lam <thanhlam0290@gmail.com>
2026-04-14 20:11:59 +0700

    Update

--- install-requiment.sh
+++ install-requiment.sh
@@ -53,10 +53,10 @@ pip install -r "$COMFY_PATH/requirements.txt" --prefer-binary
 echo "[+] Installing custom_nodes requirements..."
 
 {
-  find "$COMFY_PATH/custom_nodes" -type f -name "requirements.txt" \
+  find "$COMFY_PATH/custom_nodes" -type f -name "requirements.txt" -size +0c \
     -exec sh -c 'cat "$1"; echo' _ {} \;
 } > "$ALL_REQ"
-sed -i 's/transparent-backgrounddiffusers/transparent-background\ndiffusers/' "$ALL_REQ"
+# sed -i 's/transparent-backgrounddiffusers/transparent-background\ndiffusers/' "$ALL_REQ"
 pip install pip-tools
 
 if pip-compile "$ALL_REQ" -o "$FINAL_REQ" --resolver=backtracking \
