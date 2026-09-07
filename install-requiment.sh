e557cd125e30b31b7407e459da3c1751d5186fdc
Thanh Lam <thanhlam0290@gmail.com>
2026-09-07 16:20:01 +0700

    update

--- install-requiment.sh
+++ install-requiment.sh
@@ -47,10 +47,15 @@ rm -rf "$COMFY_PATH/custom_nodes/ComfyUI-Crystools" || true
 rm -rf "$COMFY_PATH/custom_nodes/comfyui-manager" || true
 rm -rf "$COMFY_PATH/custom_nodes/ComfyUI-Inpaint-CropAndStitch" || true
 
-git clone https://github.com/rgthree/rgthree-comfy.git "$COMFY_PATH/custom_nodes/rgthree-comfy" || true
-git clone https://github.com/ltdrdata/ComfyUI-Manager "$COMFY_PATH/custom_nodes/comfyui-manager" || true
-git clone https://github.com/crystian/ComfyUI-Crystools.git "$COMFY_PATH/custom_nodes/ComfyUI-Crystools" || true
-git clone https://github.com/lquesada/ComfyUI-Inpaint-CropAndStitch.git "$COMFY_PATH/custom_nodes/ComfyUI-Inpaint-CropAndStitch" || true
+git config --global http.version HTTP/1.1
+git config --global http.lowSpeedLimit 1000
+git config --global http.lowSpeedTime 30
+git config --global core.compression 0
+
+git clone --depth 1 https://github.com/rgthree/rgthree-comfy.git "$COMFY_PATH/custom_nodes/rgthree-comfy" || true
+git clone --depth 1 https://github.com/ltdrdata/ComfyUI-Manager "$COMFY_PATH/custom_nodes/comfyui-manager" || true
+git clone --depth 1 https://github.com/crystian/ComfyUI-Crystools.git "$COMFY_PATH/custom_nodes/ComfyUI-Crystools" || true
+git clone --depth 1 https://github.com/lquesada/ComfyUI-Inpaint-CropAndStitch.git "$COMFY_PATH/custom_nodes/ComfyUI-Inpaint-CropAndStitch" || true
 
 # ==============================
 # CHECK PYTHON & CREATE/ACTIVATE VENV
