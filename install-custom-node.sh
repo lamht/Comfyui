177400a362760ef9f63c34ce06b5b73801d51fc4
Thanh Lam <thanhlam0290@gmail.com>
2026-09-22 16:43:39 +0700

    update

--- install-custom-node.sh
+++ install-custom-node.sh
@@ -228,6 +228,8 @@ wc -l "$ALL_REQ"
 
 cp "$ALL_REQ" "$FINAL_REQ"
 
+$PYTHON -m pip install py-cpuinfo
+
 # ==============================
 # INSTALL NODE REQUIREMENTS
 # ==============================
